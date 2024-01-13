ac1ca87394c2f0d9139b7e3fb9d6928518c3f8a8
Shweta Bhagat <75883328+Shweta2024@users.noreply.github.com>
2024-01-13 11:40:37 +0530

    Create code.cpp

---
+++ January/13-Minimum Number of Steps to Make Two Strings Anagram/code.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minSteps(string s, string t)
+    {
+        unordered_map<char,int>memo;
+        
+        for(int index = 0; index < s.size(); index++)
+        {
+            memo[s[index]]++;
+            memo[t[index]]--;
+        }
+        
+        int ans = 0;
+        for(auto current : memo)
+            ans += abs(current.second);
+        
+        return ans/2;
+    }
+};
+
+// Time Complexity = O(N+M)
+//Space Complexity = O(N+M)
